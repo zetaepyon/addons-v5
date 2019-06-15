1145532cb7f8201d11a9edd558bab5cc66d3dd71
zetaepyon <antilles.zeta+github@gmail.com>
2019-06-14 22:49:24 -0500

    Update clock
    Remove unused dependencies

--- clock/clock.lua
+++ clock/clock.lua
@@ -1,15 +1,12 @@
 local os = require('os')
-local math = require('math')
 local table = require('table')
-local string = require('string')
 local ui = require('ui')
-local list = require('lists')
 local settings = require('settings')
 
 local time_zones = require('time_zones')
 local local_offset = os.date('%z')/100
 
-defaults = {}
+local defaults = {}
 defaults.pos = {}
 defaults.pos.x = 1555
 defaults.pos.y = 23
@@ -20,9 +17,9 @@ defaults.separator = ' | '
 defaults.sort = 'Time'
 defaults.timezones = {'CST','PST','JST',}
 
-options = settings.load(defaults)
+local options = settings.load(defaults)
 
-local sort = {    
+local sort = {
     time = function(t1, t2)
         return time_zones[t1] < time_zones[t2]
     end,
@@ -36,13 +33,13 @@ options.timezones = options.sort ~= 'None' and table.sort(options.timezones,sort
 ui.display(function()
 
     local clock = {'['}
-    
+
     for i,v in ipairs(options.timezones) do
 
         local tz_name = ' '..v
         local tz_offset = time_zones[v]
         local tz_time = os.date(options.format, os.time() + ((tz_offset - local_offset) * 3600))
-        
+
         if i ~= 1 then table.insert(clock, options.separator) end
         table.insert(clock, tz_time)
         if options.showtimezones then table.insert(clock, tz_name) end
